406cf39eeed3ee17fd2e8ff50d21fa60f0be17d8
mattgrobelny <mattgrobelny@gmail.com>
2016-09-27 13:18:15 -0500

    median count works

--- Grobelny_HW4.py
+++ Grobelny_HW4.py
@@ -1,5 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python/2.7.9
+# Runs on python/2.7.9 from cluster
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Grobelny HW 4
 
@@ -16,6 +19,10 @@ def progress(count, total, suffix=''):
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', suffix))
     sys.stdout.flush()
 
+def convert_phred(asiic_score):
+        q_score_for_nuc = ord(str(asiic_score)) - 33
+        return q_score_for_nuc
+
 print "constructing arrays\n"
 # make 2d array
 qual_list = []
@@ -30,30 +37,32 @@ var_std_dev_list.append([])  # [1] = variance totals
 var_std_dev_list.append([])  # [2] = variance
 var_std_dev_list.append([])  # [3] = standard dev
 
-# make 3rd 2d array
-median_list = []
-median_list.append([])  # [0] = base number
-median_list.append([])  # [1] = median val dictionary
-median_list.append([])  # [2] = median val
+median_list_output = []
+median_list_output.append([])
+median_list_output.append([])
+
 
 for i in range(101):
     # add base numbers to each array
     qual_list[0].append(i)
     var_std_dev_list[0].append(i)
-    median_list[0].append(i)
+    median_list_output[0].append(i)
     # intialize array values
     qual_list[1].append(0.0)
     qual_list[2].append(0.0)
     var_std_dev_list[1].append(0.0)
     var_std_dev_list[2].append(0.0)
     var_std_dev_list[3].append(0.0)
-    median_list[1].append({})
-    median_list[2].append(0)
-
+    median_list_output[1].append(0.0)
 
 # open file
-in_file = "/home/a-m/ib501_stud12/shell/lane1_NoIndex_L001_R1_003.fastq"
+# in_file = "/home/a-m/ib501_stud12/shell/lane1_NoIndex_L001_R1_003.fastq"
+in_file = "/home/a-m/ib501_stud12/shell/lane1.short.fastq"
 fh1 = open(in_file, 'r')
+
+# file length
+#file_length = 4000000 * 4
+file_length = 1000
 
 # line counter
 count = 0
@@ -72,10 +81,10 @@ for line in fh1:
     if count % 4 == 1:
         record_count += 1
         # update progress bar
-        progress(count, 4000000 * 4, suffix='Percent done')
+        progress(count, file_length, suffix='Percent done')
         for i in range(101):
             # convert ASCII to ints offset by 33
-            qual_score = ord(str(line[i])) - 33
+            qual_score = convert_phred(line[i])
 
             # add up qual scores at base number
             qual_list[1][i] = qual_list[1][i] + float(qual_score)
@@ -90,26 +99,27 @@ for i in range(101):
 
 print qual_list
 print count
+
 ###############################################################################
+# Part 2
 # open file
-in_file2 = "/home/a-m/ib501_stud12/shell/lane1_NoIndex_L001_R1_003.fastq"
-fh2 = open(in_file2, 'r')
+fh2 = open(in_file, 'r')
 
 # skip record name, seq, quality name
 next(fh2)
 next(fh2)
 next(fh2)
 count2 = 0
+
 print "\nCalculating variance and standard dev per base..."
 for line in fh2:
     count2 = count2 + 1
-    if count % 4 == 1:
+    if count2 % 4 == 1:
         # update progress bar
-        progress(count2, 4000000 * 4, suffix='Percent done')
+        progress(count2, file_length, suffix='Percent done')
         for i in range(101):
             # convert ASCII to ints offset by 33
-            print line[i], qual_list[2][i]
-            variance_score = abs((ord(str(line[i])) - 33) - qual_list[2][i])
+            variance_score = abs(convert_phred(line[i]) - qual_list[2][i])
             # add up qual scores at base number
             var_std_dev_list[1][i] = var_std_dev_list[1][i] + float(variance_score ** 2)
 
@@ -121,23 +131,71 @@ for i in range(101):
     # Calc variance
     var_std_dev_list[2][i] = float(var_std_dev_list[1][i]) / float(record_count)
     # Calc standard dev
-    var_std_dev_list[3][i] = float(sqrt(var_std_dev_list[2][i]))
+    var_std_dev_list[3][i] = float(np.sqrt(var_std_dev_list[2][i]))
 print var_std_dev_list
 
 
 ###############################################################################
-# import matplotlib.pyplot as plt
-#
-# # Plot data
-# plt.plot(qual_list[0], qual_list[2])
-#
-# # Add labels
-# plt.xlabel('Base #')
-# plt.ylabel('Quality Score (phred)')
-# plt.title('Avg Quality per Base')
-# plt.grid(True)
-#
-# print "Saving Plot of: avg_qual_per_base.png"
-# # Save first graph
-# plt.savefig('avg_qual_per_base.png')
+# Part 3
+
+median_list = []
+
+for i in range(101):
+    median_list.append([])
+
+# open file
+fh3 = open(in_file, 'r')
+
+# skip record name, seq, quality name
+next(fh3)
+next(fh3)
+next(fh3)
+count3 = 0
+print "\nCalculating median quality per base..."
+for line in fh3:
+    count3 = count3 + 1
+    if count3 % 4 == 1:
+        # update progress bar
+        progress(count3, file_length, suffix='Percent done')
+        for i in range(101):
+            median_list[i].append(convert_phred(line[i]))
+    else:
+        continue
+fh3.close
+
+for i in range(101):
+    median_list_output[1][i] = np.median(median_list[i])
+#print median_list[1]
+print median_list_output
+
+
+###############################################################################
+# Plot data
+plt.errorbar(qual_list[0], qual_list[2], yerr=var_std_dev_list[3], fmt='o')
+
+# Add labels
+plt.xlabel('Base #')
+plt.ylabel('Quality Score (phred)')
+plt.title('Avg Quality per Base')
+plt.grid(True)
+
+print "Saving Plot of: avg_qual_per_base.png"
+# Save first graph
+plt.savefig('avg_qual_per_base.png')
+
+
+# Plot data
+for i in [5, 95]:
+    xlist = range(43)
+    plot.hist(xlist, median_list[i])
+
+    # Add labels
+    plt.xlabel('Quality Score (phred)')
+    plt.ylabel('Quality Score Freq')
+    plt.title('Distribution of Quality scores at %s Base') % (i)
+    plt.grid(True)
+
+    print "Saving Plot of: dis_of_qual_at_base%s.png" % (i)
+    # Save first graph
+    plt.savefig("dis_of_qual_at_base%s.png") % (i)
 ###############################################################################
