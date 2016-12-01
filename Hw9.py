092be8bb31de9159cb5beba2226214acf753adee
mattgrobelny <mattgrobelny@gmail.com>
2016-12-01 16:57:57 -0600

    finished arc drawing fuc
    need to test

--- Hw9.py
+++ Hw9.py
@@ -5,19 +5,23 @@ import mysql.connector
 #
 
 ### Start up connections to both DBs
+
 dbh_s12 = mysql.connector.connect(user='s12', password='jazzduck', database='s12')
 cursor_s12 = dbh_s12.cursor()
+test1=("SELECT * FROM Blast_output LIMIT 10;")
+cursor_s12.execute(test1)
 
 dbh_gene_db = mysql.connector.connect(user='s12', password='jazzduck', database='gene_db')
 cursor_gene_db = dbh_gene_db.cursor()
-
+test2=("SELECT * FROM gene_db LIMIT 10;")
+cursor_gene_db.execute(test2)
 # Set up queries for both DBs
 
 ###############################################################################
 # Queries for DB S12
 
 # Collect Unique blast hitting genes
-blast_hitting_genes_query = ("SELECT DISTINCT qseqid FROM Blast_out;")
+blast_hitting_genes_query = ("SELECT DISTINCT qseqid FROM Blast_out")
 
 query_blast_out = ("SELECT sseqid, evalue FROM gene WHERE qseqid=%s AND MIN(evalue)")
 
@@ -36,7 +40,6 @@ transcript_seq_blast_hit = ("SELECT sequence FROM transcript WHERE gene_id=%s")
 ###############################################################################
 
 print "# ----- Blast Output ----- #"
-
 for gene_id in blast_hitting_genes_query_output:
 
     # Print out actual gene id
@@ -52,3 +55,24 @@ dbh_s12.close()
 
 cursor_gene_db.close()
 dbh_gene_db.close()
+
+import mysql.connector
+#
+# Connect and get a database handle.
+#
+dbh    = mysql.connector.connect(user='s12', password='jazzduck',database='gene_db')
+cursor = dbh.cursor()
+#
+# Form the query with placeholders for variables.
+#
+query  = ("SELECT gene_id, chromosome, start_base " \
+"FROM gene "\
+"WHERE chromosome=%s AND start_base>%s")
+#
+# Execute the query.
+#
+cursor.execute(query, ("17", 10000000))
+for (gene_id, chr, bp) in cursor:
+    print "Gene ID:", gene_id, "; Chromosome:", chr, "; start bp:", bp
+cursor.close()
+dbh.close()
