d57f78900f8d8b1a43e52a250833eb1b773dda5b
mattgrobelny <mattgrobelny@gmail.com>
2016-12-21 22:47:09 -0600

    update hw9 and delete old data viz file

--- Hw9.py
+++ Hw9.py
@@ -15,19 +15,21 @@ cursor_gene_db = dbh_gene_db.cursor()
 # Queries for DB S12
 
 # Collect Unique blast hitting genes
-blast_hitting_genes_query = ("SELECT DISTINCT qseqid FROM Blast_output")
+blast_hitting_genes_query = ("SELECT qseqid FROM Blast_output")
 
 # collect lowest evalue blast hit per gene
-query_blast_out = ("SELECT sseqid, evalue FROM Blast_output WHERE qseqid REGEXP "'%s'" ORDER BY evalue ASC LIMIT 1;")
+query_blast_out = ("SELECT qseqid, sseqid, evalue FROM Blast_output WHERE qseqid REGEXP "'%s'" ORDER BY evalue ASC LIMIT 1;")
 
 cursor_s12.execute(blast_hitting_genes_query)
 
-# capture list of hitting genes
+# capture list of hitting genes and corresponding trans ids
 hits_gene_list = []
 for value in cursor_s12:
     split_val = value[0].split("_")
-    hits_gene_list.append(split_val)
-
+    if split_val[0] not in hits_gene_list:
+        hits_gene_list.append(split_val[0])
+    else:
+        continue
 ###############################################################################
 # Queries for DB gene_db
 
@@ -42,20 +44,23 @@ print "Gene id", '\t', "Trans_id", '\t' "Blast hit", '\t', "E-value"', \t', "Tra
 for gene_id in hits_gene_list:
 
     # Query lowest evalue blast hit
-    gene_to_search = '^' + str(gene_id[0])
+    gene_to_search = '^' + str(gene_id)
     cursor_s12.execute(query_blast_out, (gene_to_search,))
-    blast_hit = cursor_s12.fetchall()  # two outputs hit seq and eval
+    blast_hit = cursor_s12.fetchall()  # three outputs: gene_id+trans_id, hit seq, and eval
+
+    # recapture trans id from lowest evalue blast hit
+    trans_id_val = blast_hit[0][0].split("_")
 
     # Query transcript
-    cursor_gene_db.execute(transcript_seq_blast_hit, (gene_id[1],))
+    cursor_gene_db.execute(transcript_seq_blast_hit, (trans_id_val[1],))
     transcript_out = cursor_gene_db.fetchall()
 
     # output in tsv
-    print gene_id[0], '\t', gene_id[1], '\t', blast_hit[0][0], '\t', blast_hit[0][1], '\t', transcript_out[0][0]
+    print gene_id, '\t', trans_id_val[1], '\t', blast_hit[0][1], '\t', blast_hit[0][2], '\t', transcript_out[0][0]
 
 # Closes out connections
 cursor_s12.close()
 dbh_s12.close()
 
 cursor_gene_db.close()
-dbh_gene_db.close()
+dbh_gene_db.close()  # DONE!
